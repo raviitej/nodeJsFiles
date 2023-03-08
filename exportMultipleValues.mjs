5900c8261ddab096778748699d9cbf9591fd4d42
raviitej <pandateja237@gmail.com>
2023-03-08 18:56:51 +0000

    again updated export

--- exportMultipleValues.mjs
+++ exportMultipleValues.mjs
@@ -1,4 +1,3 @@
-let myArray = ["camel", 265, true, "5.6"];
-let bulb = { watts: 10, type: "LED" };
-let multiplyByFour = (num) => 4 * num;
-export { myArray, bulb, multiplyByFour };
+export let myArray = ["camel", 265, true, "5.6"];
+export let bulb = { watts: 10, type: "LED" };
+export let multiplyByFour = (num) => 4 * num;
